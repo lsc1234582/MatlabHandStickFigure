26bc75b46dd46f3210edfcfe130481c661ddc673
lsc1234582 <meneerli@hotmail.com>
2016-08-26 20:26:17 +0100

    loadAndSave now works with existing directory structure

--- HandStickFigure/loadAndSave.m
+++ HandStickFigure/loadAndSave.m
@@ -1,12 +1,16 @@
 clear;
-files = dir('2*');
+roots = {'AW_01', 'AW_03', 'AW_05'};
+subs = {'Kitchen', 'Bedroom'};
 fileName = 'NothingZeroedMergedCodeWithLoadObjTSCorrectedGazeSyncedDec.mat';
-for path = files'
-    file = [path.name, '/', fileName];
-    cal = [path.name, '/', 'RightHand.cal'];
-    outputFile = [path.name, '/', 'RightHand.avi'];
-    load(file);
-    playBackBVH(calibrateData(F.RightHand.Data(12000:13000, :), cal)*180/pi, ...
+for root = roots
+   for sub = subs
+      path = [cell2mat(root), '/', cell2mat(sub), '/'];
+      file = [path, fileName];
+      outputFile = [path, 'RightHand'];
+      load(file);
+      cal = strrep(F.RecordDate, '-', '');
+      cal = ['HandCalibrationsonly/', cal, '1/RightHand.cal']
+      playBackBVH(calibrateData(F.RightHand.Data, cal)*180/pi, ...
         'HandBase3.bvh', F.RightHand.Fs, 'outputName', outputFile);
-    
+   end
 end
